37108d6a2ef8530bdf9f874ea4076e5fbd9f9996
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-01-23 00:15:20 +0100

    checkpoint: adjustments and cleanup for infinite floor

--- src/entities/InfiniteFloor.ts
+++ src/entities/InfiniteFloor.ts
@@ -1,4 +1,12 @@
-import { Texture, Vector3, BufferGeometry, Mesh } from "three";
+import {
+  Texture,
+  Vector3,
+  Mesh,
+  DataTexture,
+  FloatType,
+  LinearFilter,
+  RedFormat,
+} from "three";
 import {
   ColliderDesc,
   HeightFieldFlags,
@@ -7,22 +15,9 @@ import {
   World,
 } from "@dimforge/rapier3d-compat";
 import { State } from "../core/Engine";
-import {
-  BufferAttribute,
-  DataTexture,
-  FloatType,
-  InstancedMesh,
-  Line,
-  LinearFilter,
-  MeshBasicMaterial,
-  MeshLambertNodeMaterial,
-  Object3D,
-  RedFormat,
-  SphereGeometry,
-} from "three/webgpu";
+import { MeshLambertNodeMaterial } from "three/webgpu";
 import floor_TEMPORARY_TextureUrl from "/environment/heightmap-1024.webp?url";
 import floorModelUrl from "/environment/floor.glb?url";
-// import mapHeightfieldModelUrl from "/environment/model-heightmap.glb?url";
 import mapHeightfieldModelUrl from "/environment/model-heightmap-displacements.glb?url";
 import {
   clamp,
@@ -39,7 +34,6 @@ import {
   vec3,
 } from "three/tsl";
 import { GLTF } from "three/examples/jsm/Addons.js";
-import Grass from "../entities/Grass";
 
 export default class InfiniteFloorInstanced {
   private readonly HALF_FLOOR_THICKNESS = 0.3;
@@ -47,87 +41,67 @@ export default class InfiniteFloorInstanced {
   private readonly HALF_MAP_SIZE = this.MAP_SIZE / 2;
   private readonly KINTOUN_ACTIVATION_THRESHOLD = 2;
 
-  private heightmapTexture?: DataTexture;
+  private displacementTexture = new DataTexture();
+  private uDisplacement = uniform(0);
 
-  private offset = 0;
-
-  // Mesh
-  private floorTexture: Texture;
+  private floorTexture = new Texture();
   private floor!: Mesh;
 
-  // Physics
   private kintounRigidBody: RigidBody; // Kintoun = Flying Nimbus cloud from dragon ball
-
-  // private grass: Grass;
 
   private uTime = uniform(0);
 
   constructor(state: State) {
-    const { scene, world, assetManager } = state;
+    const { world } = state;
 
-    (this.floorTexture = assetManager.textureLoader.load(
-      floor_TEMPORARY_TextureUrl,
-      (t) => {
-        t.flipY = false;
-      },
-    )),
-      (async () => {
-        await Promise.all([
-          assetManager.gltfLoader.loadAsync(floorModelUrl).then((model) => {
-            this.floor = this.createFloorFromModel(model);
-            scene.add(this.floor);
-          }),
-          assetManager.gltfLoader
-            .loadAsync(mapHeightfieldModelUrl)
-            .then((model) => {
-              // const geom = new BufferGeometry();
-              // const { position } = (model.scene.children[0] as Mesh).geometry
-              //   .attributes;
-              // geom.setAttribute("position", position);
-              // const lines = new Line(
-              //   geom,
-              //   new MeshBasicMaterial({ color: "red" }),
-              // );
-              // scene.add(lines);
-              // const instances = new InstancedMesh(
-              //   new SphereGeometry(0.25),
-              //   new MeshBasicMaterial({ color: "red" }),
-              //   position.count,
-              // );
-
-              // const dummy = new Object3D();
-              // for (let i = 0; i < position.count; i++) {
-              //   const i3 = i * 3;
-              //   const x = position.array[i3];
-              //   const y = position.array[i3 + 1];
-              //   const z = position.array[i + 2];
-              //   dummy.position.set(x, y, z);
-              //   dummy.updateMatrix();
-              //   instances.setMatrixAt(i, dummy.matrix);
-              // }
-              // instances.instanceMatrix.needsUpdate = true;
-              // scene.add(instances);
-
-              this.createHeightmapCollider(model, world);
-            }),
-        ]);
-      })();
+    this.loadFloorTexture(state); // temporary
+    this.loadDisplacementModel(state);
+    this.loadFloorModel(state);
 
     this.kintounRigidBody = this.createKintounCollider(world);
-
-    // this.grass = new Grass(state);
   }
 
-  private createFloorFromModel(model: GLTF) {
-    const mesh = model.scene.children[0] as Mesh;
+  /**
+   *
+   * @param Note For best result plane should be:
+   * - a square of size MAP_SIZExMAP_SIZE
+   * - have only positive values for y (y>=0)
+   * - be centered meaning it ranges from [-half, +half] for both x and z
+   */
+  private loadDisplacementModel(state: State) {
+    const { assetManager, world } = state;
+    assetManager.gltfLoader.load(mapHeightfieldModelUrl, (model) => {
+      this.createMapCollider(model, world);
+    });
+  }
+
+  private loadFloorTexture(state: State) {
+    const { assetManager } = state;
+    assetManager.textureLoader.load(floor_TEMPORARY_TextureUrl, (texture) => {
+      texture.flipY = false;
+      this.floorTexture.copy(texture);
+    });
+  }
+
+  private loadFloorModel(state: State) {
+    const { assetManager, scene } = state;
+    assetManager.gltfLoader.load(floorModelUrl, (model) => {
+      this.floor = this.createFloorFromModel(model);
+      scene.add(this.floor);
+    });
+  }
+
+  private createFloorFromModel(floorModel: GLTF) {
+    const mesh = floorModel.scene.children[0] as Mesh;
     mesh.receiveShadow = true;
     const material = this.createFloorMaterial();
     mesh.material = material;
     return mesh;
   }
 
-  private getHeightfieldDataFromModel(model: GLTF) {
+  private extractDisplacementDataFromModel(model: GLTF) {
     const mesh = model.scene.children[0] as Mesh;
+    const displacement = mesh.geometry.attributes._displacement.array[0]; // they are all the same
     const positionAttribute = mesh.geometry.attributes.position;
     if (!mesh.geometry.boundingBox) mesh.geometry.computeBoundingBox();
     const boundingBoxAttribute = mesh.geometry.boundingBox!;
@@ -144,48 +118,52 @@ export default class InfiniteFloorInstanced {
       const y = positionAttribute.array[i * 3 + 1]; // in [0, someHeight]
       const z = positionAttribute.array[i * 3 + 2]; // in [-halfExtent..+halfExtent]
 
-      // Map x from [-64..+64] to [0..1] => index in [0..(rowsCount - 1)]
+      // Map x from [-halfExtent..+halfExtent] to [0..1] => index in [0..(rowsCount - 1)]
       const indexX = Math.round((x / (halfExtent * 2) + 0.5) * (rowsCount - 1));
       const indexZ = Math.round((z / (halfExtent * 2) + 0.5) * (rowsCount - 1));
 
-      // row-major: row = indexZ, col = indexX
+      // col-major: row = indexZ, col = indexX
       const index = indexX + indexZ * rowsCount;
 
-      // Store the y
       heights[index] = y;
     }
+    this.uDisplacement.value = displacement;
 
-    const offset = boundingBoxAttribute.max.y / 2;
-    this.offset = offset;
-
-    return { rowsCount, heights, offset };
+    return { rowsCount, heights, displacement };
   }
 
-  private createHeightmapCollider(model: GLTF, world: World) {
-    // 1) Extract height data
-    const { rowsCount, heights, offset } =
-      this.getHeightfieldDataFromModel(model);
-
-    // 2) Create a fixed rigid body, translated by offsetY if needed
-    const rigidBodyDesc = RigidBodyDesc.fixed().setTranslation(0, -offset, 0);
-    const rigidBody = world.createRigidBody(rigidBodyDesc);
-
-    // 3) Create a DataTexture for optional GPU displacement in your shader
-    this.heightmapTexture = new DataTexture(
+  private createDisplacementDataTexture(
+    displaceMentData: ReturnType<typeof this.extractDisplacementDataFromModel>,
+  ) {
+    const { rowsCount, heights } = displaceMentData;
+    const data = new DataTexture(
       heights,
       rowsCount,
       rowsCount,
       RedFormat,
       FloatType,
     );
-    this.heightmapTexture.minFilter = LinearFilter;
-    this.heightmapTexture.magFilter = LinearFilter;
-    this.heightmapTexture.generateMipmaps = false;
-    this.heightmapTexture.needsUpdate = true;
+    data.minFilter = LinearFilter;
+    data.magFilter = LinearFilter;
+    data.generateMipmaps = false;
+    data.needsUpdate = true;
 
-    // 4) Define the Rapier collider
-    //    - We pass (rows-1, cols-1) = (64, 64) for the number of "cells"
-    //    - The "scale" param will map 64 => 300 units in X,Z
+    this.displacementTexture.copy(data);
+  }
+
+  private createHeightfieldCollider(
+    displaceMentData: ReturnType<typeof this.extractDisplacementDataFromModel>,
+    world: World,
+  ) {
+    const { rowsCount, heights, displacement } = displaceMentData;
+
+    const rigidBodyDesc = RigidBodyDesc.fixed().setTranslation(
+      0,
+      -displacement,
+      0,
+    );
+    const rigidBody = world.createRigidBody(rigidBodyDesc);
+
     const colliderDesc = ColliderDesc.heightfield(
       rowsCount - 1,
       rowsCount - 1,
@@ -202,6 +180,12 @@ export default class InfiniteFloorInstanced {
 
     // 5) Finally, create the collider in Rapier
     world.createCollider(colliderDesc, rigidBody);
+  }
+
+  private createMapCollider(model: GLTF, world: World) {
+    const displaceMentData = this.extractDisplacementDataFromModel(model);
+    this.createDisplacementDataTexture(displaceMentData);
+    this.createHeightfieldCollider(displaceMentData, world);
   }
 
   private createKintounCollider(world: World) {
@@ -226,11 +210,11 @@ export default class InfiniteFloorInstanced {
   }
 
   private material_applyMapDisplacement = Fn(([uv = vec2(0, 0)]) => {
-    const displacedY = texture(this.heightmapTexture!, uv).r;
+    const displacedY = texture(this.displacementTexture, uv).r;
 
     const displacedPosition = vec3(
       positionLocal.x,
-      displacedY.sub(this.offset),
+      displacedY.sub(this.uDisplacement),
       positionLocal.z,
     );
 
@@ -252,17 +236,15 @@ export default class InfiniteFloorInstanced {
     const outsideMapColor = color("#8FBC8B");
     const insideMapColor = color("coral");
 
-    return mix(colorSample, outsideMapColor, isOutsideMap);
+    return mix(insideMapColor, outsideMapColor, isOutsideMap);
   });
 
   private createFloorMaterial() {
     const materialNode = new MeshLambertNodeMaterial();
-    // materialNode.wireframe = true;
+    materialNode.wireframe = true;
 
     // 1. Calculate the static UVs based on the rotated world position
-    const uv = positionWorld.zx
-      .add(this.HALF_MAP_SIZE - 0.0001)
-      .div(this.MAP_SIZE - 0.0002);
+    const uv = positionWorld.zx.add(this.HALF_MAP_SIZE).div(this.MAP_SIZE);
     const clampedUV = clamp(uv, 0.0, 1.0); // Prevent sampling outside the map
 
     materialNode.positionNode = this.material_applyMapDisplacement(clampedUV);
