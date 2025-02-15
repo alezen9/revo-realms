7d824263b636fc966c15ee3c2f321fd1575b6da2
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-02-15 02:57:06 +0100

    chore: add flag for enabling disabling monitoring

--- src/systems/RendererManager.ts
+++ src/systems/RendererManager.ts
@@ -10,6 +10,7 @@ export default class RendererManager {
   private monitoringManager: MonitoringManager;
   private postprocessingManager!: PostprocessingManager;
   private readonly POSTPROCESSING_ENABLED = false;
+  private readonly MONITORING_ENABLED = true;
 
   constructor() {
     const canvas = document.createElement("canvas");
@@ -20,28 +21,32 @@ export default class RendererManager {
     const renderer = new WebGPURenderer({
       canvas,
       antialias: true,
-      trackTimestamp: true,
+      trackTimestamp: false,
     });
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = VSMShadowMap;
     renderer.toneMapping = ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1.5;
     this.renderer = renderer;
-    this.monitoringManager = new MonitoringManager();
+    this.monitoringManager = new MonitoringManager(this.MONITORING_ENABLED);
   }
 
   async init(sceneManager: SceneManager) {
     this.postprocessingManager = new PostprocessingManager(this, sceneManager);
-    await this.monitoringManager.stats.init(this.renderer);
+    if (this.MONITORING_ENABLED)
+      await this.monitoringManager.stats.init(this.renderer);
   }
 
   async renderAsync(...args: Parameters<typeof this.renderer.renderAsync>) {
-    await this.renderer.resolveTimestampsAsync("compute");
+    if (this.MONITORING_ENABLED)
+      await this.renderer.resolveTimestampsAsync("compute");
     if (this.POSTPROCESSING_ENABLED)
       await this.postprocessingManager.composer.renderAsync();
     else await this.renderer.renderAsync(...args);
-    this.monitoringManager.updateCustomPanels(this.renderer);
-    await this.renderer.resolveTimestampsAsync("render");
-    this.monitoringManager.stats.update();
+    if (this.MONITORING_ENABLED) {
+      this.monitoringManager.updateCustomPanels(this.renderer);
+      await this.renderer.resolveTimestampsAsync("render");
+      this.monitoringManager.stats.update();
+    }
   }
 }
