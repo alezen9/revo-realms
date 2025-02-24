5fc80bf1975853b8287c5127fed57316c242d31a
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-02-25 00:50:53 +0100

    chore: minor optimizations for player class
    feat: add underwater color to player, still very rough tho

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,22 +1,18 @@
-import {
-  Mesh,
-  IcosahedronGeometry,
-  Vector3,
-  Quaternion,
-  Camera,
-  MeshStandardMaterial,
-} from "three";
+import { Mesh, IcosahedronGeometry, Vector3, Quaternion, Camera } from "three";
 import {
   ColliderDesc,
   RigidBody,
   RigidBodyDesc,
   World,
   Ray,
+  Vector,
 } from "@dimforge/rapier3d-compat";
 import InputManager from "../systems/InputManager";
 import { type State } from "../Game";
-import { uniform } from "three/tsl";
+import { color, float, positionWorld, step, uniform } from "three/tsl";
 import LightingSystem from "../systems/LightingSystem";
+import { MeshStandardNodeMaterial } from "three/webgpu";
+// import { UniformType } from "../types";
 
 export default class Player {
   private mesh: Mesh;
@@ -31,6 +27,7 @@ export default class Player {
 
   // Yaw (rotation around Y-axis)
   private yawInRadians = 0;
+  private prevYawInRadians = -1;
   private yawQuaternion = new Quaternion();
 
   // Jump & Movement Configuration
@@ -42,12 +39,14 @@ export default class Player {
   private readonly MAX_UPWARD_VELOCITY = 6;
   private readonly LINEAR_DAMPING = 0.25;
   private readonly ANGULAR_DAMPING = 1;
+  private jumpImpulse = new Vector3(0, this.JUMP_IMPULSE, 0);
 
   private readonly LIN_VEL_STRENGTH = 25;
   private readonly ANG_VEL_STRENGTH = 25;
   private newLinVel = new Vector3();
   private newAngVel = new Vector3();
   private torqueAxis = new Vector3();
+  private forwardVec = new Vector3();
 
   // Player State
   private isOnGround = false;
@@ -57,11 +56,15 @@ export default class Player {
 
   // Constants for geometry/camera offset
   private readonly RADIUS = 0.5;
-  private readonly PLAYER_INITIAL_POSITION = new Vector3(0, 2, 0);
+  private readonly PLAYER_INITIAL_POSITION = new Vector3(-95, 5, 45);
   private readonly CAMERA_OFFSET = new Vector3(0, 11, 17);
   private readonly CAMERA_LERP_FACTOR = 7.5;
   private readonly UP = new Vector3(0, 1, 0);
   private readonly DOWN = new Vector3(0, -1, 0);
+  private readonly FORWARD = new Vector3(0, 0, -1);
+
+  private rayOrigin = new Vector3();
+  private ray = new Ray(this.rayOrigin, this.DOWN);
 
   lighting: LightingSystem;
 
@@ -83,12 +86,8 @@ export default class Player {
 
   private createCharacterMesh() {
     const geometry = new IcosahedronGeometry(this.RADIUS, 3);
-    const material = new MeshStandardMaterial({
-      color: "silver",
-      flatShading: true,
-      metalness: 1,
-      roughness: 0.5,
-    });
+    // const material = new PlayerMaterial({ uTime: this.uTime });
+    const material = new PlayerMaterial();
     const mesh = new Mesh(geometry, material);
     mesh.castShadow = true;
     mesh.position.copy(this.PLAYER_INITIAL_POSITION);
@@ -113,6 +112,12 @@ export default class Player {
     const delta = clock.getDelta();
 
     this.uTime.value = clock.getElapsedTime();
+
+    // Build yaw quaternion
+    if (this.prevYawInRadians !== this.yawInRadians) {
+      this.yawQuaternion.setFromAxisAngle(this.UP, this.yawInRadians);
+      this.prevYawInRadians = this.yawInRadians;
+    }
 
     this.updateVerticalMovement(delta, world);
     this.updateHorizontalMovement(delta);
@@ -143,7 +148,7 @@ export default class Player {
     }
 
     // 4) Mid-air logic (jump cut, fast fall, clamp)
-    const velocity = this.rigidBody.linvel() as Vector3;
+    const velocity = this.rigidBody.linvel();
     this.handleJumpCut(isJumpKeyPressed, velocity);
     this.handleFastFall(delta, velocity, world.gravity.y);
     this.clampUpwardVelocity(velocity);
@@ -155,15 +160,11 @@ export default class Player {
 
   private checkIfGrounded(world: World): boolean {
     // Cast a ray downward from just below the sphere’s center
-    const pos = this.rigidBody.translation();
-    const rayOrigin = new Vector3(pos.x, pos.y - (this.RADIUS + 0.01), pos.z);
-    const ray = new Ray(rayOrigin, this.DOWN);
-
+    this.rayOrigin.copy(this.rigidBody.translation());
+    this.rayOrigin.y -= this.RADIUS + 0.01;
     const maxDistance = 0.2;
-    const hit = world.castRay(ray, maxDistance, true);
-
+    const hit = world.castRay(this.ray, maxDistance, true);
     if (!hit) return false;
-
     const distanceToGround = hit.timeOfImpact * maxDistance;
     return distanceToGround < 0.01;
   }
@@ -174,24 +175,23 @@ export default class Player {
   }
 
   private performJump() {
-    const impulse = new Vector3(0, this.JUMP_IMPULSE, 0);
-    this.rigidBody.applyImpulse(impulse, true);
+    this.rigidBody.applyImpulse(this.jumpImpulse, true);
     this.jumpCount += 1;
   }
 
-  private handleJumpCut(isJumpKeyPressed: boolean, velocity: Vector3) {
+  private handleJumpCut(isJumpKeyPressed: boolean, velocity: Vector) {
     const justReleasedJump = !isJumpKeyPressed && this.wasJumpHeld;
     if (!justReleasedJump || velocity.y <= 0) return;
     velocity.y *= this.JUMP_CUT_MULTIPLIER;
   }
 
-  private handleFastFall(delta: number, velocity: Vector3, gravityY: number) {
+  private handleFastFall(delta: number, velocity: Vector, gravityY: number) {
     if (velocity.y >= 0) return;
     const extraDown = this.FALL_MULTIPLIER * Math.abs(gravityY) * delta;
     velocity.y -= extraDown;
   }
 
-  private clampUpwardVelocity(velocity: Vector3) {
+  private clampUpwardVelocity(velocity: Vector) {
     if (velocity.y <= this.MAX_UPWARD_VELOCITY) return;
     velocity.y = this.MAX_UPWARD_VELOCITY;
   }
@@ -214,13 +214,9 @@ export default class Player {
     if (isLeftward) this.yawInRadians += turnSpeed * delta;
     if (isRightward) this.yawInRadians -= turnSpeed * delta;
 
-    const forwardVec = new Vector3(
-      -Math.sin(this.yawInRadians),
-      0,
-      -Math.cos(this.yawInRadians),
-    );
+    this.forwardVec.copy(this.FORWARD).applyQuaternion(this.yawQuaternion);
 
-    this.torqueAxis.crossVectors(this.UP, forwardVec).normalize();
+    this.torqueAxis.crossVectors(this.UP, this.forwardVec).normalize();
 
     this.newLinVel.copy(this.rigidBody.linvel());
     this.newAngVel.copy(this.rigidBody.angvel());
@@ -229,11 +225,11 @@ export default class Player {
     const angVelScale = this.ANG_VEL_STRENGTH * delta;
 
     if (isForward) {
-      this.newLinVel.addScaledVector(forwardVec, linVelScale);
+      this.newLinVel.addScaledVector(this.forwardVec, linVelScale);
       this.newAngVel.addScaledVector(this.torqueAxis, angVelScale);
     }
     if (isBackward) {
-      this.newLinVel.addScaledVector(forwardVec, -linVelScale);
+      this.newLinVel.addScaledVector(this.forwardVec, -linVelScale);
       this.newAngVel.addScaledVector(this.torqueAxis, -angVelScale);
     }
 
@@ -249,16 +245,11 @@ export default class Player {
   }
 
   private updateCameraPosition(camera: Camera, delta: number) {
-    // Build yaw quaternion
-    this.yawQuaternion.setFromAxisAngle(this.UP, this.yawInRadians);
-
-    // Rotate offset
-    const offset = this.CAMERA_OFFSET.clone().applyQuaternion(
-      this.yawQuaternion,
-    );
-
-    // Desired camera pos
-    this.desiredCameraPosition.copy(this.mesh.position).add(offset);
+    // Rotate desired camera pos
+    this.desiredCameraPosition
+      .copy(this.CAMERA_OFFSET)
+      .applyQuaternion(this.yawQuaternion)
+      .add(this.mesh.position);
 
     // Lerp
     const lerpFactor = this.CAMERA_LERP_FACTOR * delta;
@@ -272,9 +263,6 @@ export default class Player {
     // Assign to camera
     camera.position.copy(this.smoothedCameraPosition);
     camera.lookAt(this.smoothedCameraTarget);
-    // For testing purpose, remove smoothing
-    // camera.position.copy(desiredCameraPosition);
-    // camera.lookAt(desiredTarget);
   }
 
   get position() {
@@ -336,4 +324,46 @@ export default class Player {
 
   //   return materialNode;
   // }
+}
+
+class PlayerMaterial extends MeshStandardNodeMaterial {
+  // private _uniforms: { uTime: UniformType<number> };
+  // constructor(uniforms: { uTime: UniformType<number> }) {
+  constructor() {
+    super();
+    // this._uniforms = { ...uniforms };
+    this.createMaterial();
+  }
+
+  private createMaterial() {
+    this.flatShading = true;
+    // this.metalness = 1;
+    // this.roughness = 0.5;
+
+    const waterLevel = float(-0.15);
+
+    const underwaterFactor = step(waterLevel, positionWorld.y);
+    const abovewaterFactor = float(1).sub(underwaterFactor);
+
+    const underwaterTint = color(0.7, 0.9, 1).mul(underwaterFactor);
+    const baseColor = color("silver").mul(abovewaterFactor);
+
+    const tintedColor = baseColor.add(underwaterTint);
+
+    // const noiseValue = texture(
+    //   assetManager.randomNoiseTexture,
+    //   uv().mul(5).add(this._uniforms.uTime.mul(0.1)),
+    // ).r;
+    // const outlineColor = color(1, 0, 0);
+    // const timer = this._uniforms.uTime.mul(0.01);
+    // const threshold = float(0.1).mul(sin(timer.add(noiseValue.mul(2.0))));
+    // const factorA = step(waterLevel.sub(threshold), positionWorld.y);
+    // const factorB = float(1).sub(
+    //   step(waterLevel.add(threshold), positionWorld.y),
+    // );
+    // const outlineFactor = factorA.mul(factorB);
+    // const finalColor = mix(tintedColor, outlineColor, outlineFactor);
+
+    this.colorNode = tintedColor;
+  }
 }
