8cb84b5bb3fba294b52cdfa0caff8cde16c94082
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-09-11 15:30:41 +0200

    chore: just started, theres nothing yet but goal is to use tiles

--- src/entities/Vegetation/Vegetation.ts
+++ src/entities/Vegetation/Vegetation.ts
@@ -2,10 +2,12 @@ import Grass from "./Grass";
 import Flowers from "./Flowers";
 import { WaterLilies } from "./WaterLilies";
 import Trees from "./Trees";
+import NewGrass from "./NewGrass";
 
 export default class Vegetation {
   constructor() {
-    new Grass();
+    // new Grass();
+    new NewGrass();
     new WaterLilies();
     new Flowers();
     new Trees();
