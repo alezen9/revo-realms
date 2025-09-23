b2c24e6b515977416bc10b6a2c18f142096fa856
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-09-23 23:15:12 +0200

    chore: implement refresh rate cap but currently unused

--- src/Game.ts
+++ src/Game.ts
@@ -6,6 +6,8 @@ import { physicsManager } from "./systems/PhysicsManager";
 import { rendererManager } from "./systems/RendererManager";
 import { eventsManager } from "./systems/EventsManager";
 import { debounce } from "lodash-es";
+import { debugManager } from "./systems/DebugManager";
+import { getRefreshRate } from "./utils/getRefreshRate";
 
 export type State = {
   clock: Clock;
@@ -21,10 +23,26 @@ export type Sizes = {
 
 export default class Game {
   private player: Player;
+  private ENABLE_CAP_FPS = false;
+  private config = {
+    halvenFPS: false,
+  };
 
   constructor() {
     this.player = new Player();
     new PortfolioRealm();
+
+    this.debugGame();
+  }
+
+  private debugGame() {
+    const folder = debugManager.panel.addFolder({
+      title: "⚡️ Performance",
+      expanded: false,
+    });
+    folder.addBinding(this.config, "halvenFPS", {
+      label: "Halven FPS",
+    });
   }
 
   private getSizes(): Sizes {
@@ -38,28 +56,38 @@ export default class Game {
     };
   }
 
+  private async updateRefreshRate() {
+    if (!this.ENABLE_CAP_FPS) return;
+    const refreshRate = await getRefreshRate();
+    this.config.halvenFPS = refreshRate >= 120;
+  }
+
   private onResize() {
     const sizes = this.getSizes();
     eventsManager.emit("resize", sizes);
+    this.updateRefreshRate();
   }
 
   async startLoop() {
+    await this.updateRefreshRate();
     const clock = new Clock(true);
 
-    const state: State = {
-      clock,
-      player: this.player,
-    };
+    const state: State = { clock, player: this.player };
 
-    const loop = async () => {
-      if (import.meta.env.DEV) sceneManager.update();
+    let flip = false;
+
+    const loop = () => {
       physicsManager.update();
-      eventsManager.emit("update", state);
-
-      rendererManager.renderAsync();
+      if (this.config.halvenFPS) flip = !flip;
+      else flip = false;
+      if (flip || !this.config.halvenFPS) {
+        if (import.meta.env.DEV) sceneManager.update();
+        eventsManager.emit("update", state);
+        rendererManager.renderAsync();
+      }
     };
 
-    // On resize
+    // resize & start
     const debouncedResize = debounce(this.onResize.bind(this), 300);
     this.onResize();
     const resizeObserver = new ResizeObserver(debouncedResize);
