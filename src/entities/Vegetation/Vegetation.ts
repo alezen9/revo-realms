f96f1c9d96e94240e358550a53b0a09d61dd5328
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-10-30 11:42:32 +0100

    experiment: tried the tiled solution for grass but experiment ended, probably not possible atm in an efficient way, better one tile

--- src/entities/Vegetation/Vegetation.ts
+++ src/entities/Vegetation/Vegetation.ts
@@ -2,11 +2,13 @@ import Grass from "./Grass";
 import Flowers from "./Flowers";
 import { WaterLilies } from "./WaterLilies";
 import Trees from "./Trees";
+import GrassTiles from "./GrassTiles/GrassField";
 // import Leaves from "./Leaves";
 
 export default class Vegetation {
   constructor() {
-    new Grass();
+    new GrassTiles();
+    // new Grass();
     // new WaterLilies();
     // new Flowers();
     // new Trees();
