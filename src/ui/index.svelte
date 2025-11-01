5cb2757b777e06486211029bcfe78eb2fd6c3008
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-11-01 20:47:36 +0100

    feat: setup svelte + add test components

---
+++ src/ui/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Test from "./Test.svelte"
+</script>
+
+<div class="ui-root">
+	<Test />
+</div>
+
+<style>
+	.ui-root {
+		position: fixed;
+		inset: 0;
+		z-index: 9999;
+		pointer-events: none; /* pass-through by default */
+	}
+	/* Any child that needs interaction should set pointer-events:auto */
+</style>
