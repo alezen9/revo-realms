3c6eaa08ca6737a52d7be2f3bcac22d387ffe47c
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2026-04-03 23:51:39 +0200

    chore: port compass to svelte

--- src/realm/RevoRealm.ts
+++ src/realm/RevoRealm.ts
@@ -1,14 +1,12 @@
 import CoolStuff from "../entities/CoolStuff/CoolStuff";
 import Terrain from "../entities/Terrain";
 import Vegetation from "../entities/Vegetation/Vegetation";
-import { Compass } from "../entities/Compass";
 import { LakeSurface } from "../entities/LakeSurface";
 import { Campfire } from "../entities/Campfire";
 export { realmConfig } from "./config";
 
 export default class PortfolioRealm {
   constructor() {
-    new Compass();
     new Terrain();
     new Vegetation();
     new CoolStuff();
